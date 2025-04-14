f23bc6100161db9caee75a2016b9a368da79cdbb
david <david.duong051@gmail.com>
2025-04-13 23:50:03 -0500

    iterative code progress...

--- Secondary Barrier.py
+++ Secondary Barrier.py
@@ -36,3 +36,11 @@ def H_leak_unshielded(W_L, d_leakage): # Calculates UNSHIELDED equivalent dose f
     '''
     H_leak_uns = ((W_L*10**-3) / d_leakage**2) * 10**3 # 10**-3 is the included barriers from the head and 10**3 converts Sv to mSv
     return H_leak_uns # (mSv/wk)
+
+
+def find_t_secondary(P, T):
+    thickness = 10 # cm
+    H_total_shielded = 100000
+    while (P/T) < H_total_shielded:
+        H_total_shielded = ((10**-(thickness/TVLa) * (W_a * a_a * F) / (d_sec_a**2 * d_sca_a**2 * 400) * 10**3) +
+                            (10**-(thickness/TVLb) * (W_b * a_b * F) / (d_sec_b**2 * d_sca_b**2 * 400) * 10**3))
