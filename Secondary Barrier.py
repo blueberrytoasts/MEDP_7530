f5261d1c76d8658e65098a963a07c7232fe5a968
david <david.duong051@gmail.com>
2025-04-13 23:22:00 -0500

    worked on 1 barrier code

---
+++ Secondary Barrier.py
@@ -0,0 +1,38 @@
+import math
+
+def B1(thickness, TVL): # Calculates B factor using only 1 TVL. Important for PATIENT SCATTER
+    '''
+    param thickness: thickness of barrier (cm)
+    param TVL: TVL (cm) where it is ALWAYS dependent on energy. Can also depend on the angle too
+    '''
+    B = 10**-(thickness/TVL)
+    return B
+
+def B2(thickness, TVL1, TVLe): # Calculates B factor using 2 TVLs. Important for LEAKAGE
+    '''
+    param thickness: thickness of barrier (cm)
+    param TVL1: first TVL (cm)
+    param TVLe: equilibrium TVL (cm)
+    '''
+    B = 10**-(1+((thickness-TVL1)/TVLe))
+    return B
+
+def H_ps_unshielded(W, d_secondary, d_scatter, a, F): # Calculates unshielded equivalent dose for PATIENT SCATTER
+    '''
+    param W: workload in a typical week (Gy/wk)
+    param d_secondary: distance from isocenter to the point of interest (cm). Includes the 0.3 m
+    param d_scatter: distance from target to isocenter. Usually 1 m
+    param a: scatter fraction dependent on angle (deg) and beam energy (MV)
+    param F: field size in cm^2
+    '''
+    H_ps_uns = (W * a * F) / (d_secondary**2 * d_scatter**2 * 400) * 10**3
+    return H_ps_uns # (mSv/wk)
+
+
+def H_leak_unshielded(W_L, d_leakage): # Calculates UNSHIELDED equivalent dose for LEAKAGE
+    '''
+    param W_L: leakage workload (Gy/wk)
+    param d_leakage: distance from source of leakage to point of protection (cm)
+    '''
+    H_leak_uns = ((W_L*10**-3) / d_leakage**2) * 10**3 # 10**-3 is the included barriers from the head and 10**3 converts Sv to mSv
+    return H_leak_uns # (mSv/wk)
