eabc7db98d6feb586f8129e568d951d73fbbaca9
david <david.duong051@gmail.com>
2025-04-21 18:34:04 -0500

    update

--- Secondary Barrier.py
+++ Secondary Barrier.py
@@ -1,104 +1,109 @@
-import math
 import csv
+import os
 
-def B1(thickness, TVL):  # Calculates B factor using only 1 TVL. Important for PATIENT SCATTER
+def find_t_secondary(P, T, F, dist, d_sca, filename='secondary_shielding.csv'):
     """
-    param thickness: thickness of barrier (cm)
-    param TVL: TVL (cm) where it is ALWAYS dependent on energy. Can also depend on the angle too
+    Calculates required secondary barrier thickness and stores the results in a CSV file.
     """
-    B = 10 ** -(thickness / TVL)
-    return B
 
+    wall = input("Which wall? ")
+    energy_choice = input("Which energy? (6, 18, or both): ").strip()
 
-def B2(thickness, TVL1, TVLe):  # Calculates B factor using 2 TVLs. Important for LEAKAGE
-    """
-    param thickness: thickness of barrier (cm)
-    param TVL1: first TVL (cm)
-    param TVLe: equilibrium TVL (cm)
-    """
-    B = 10 ** -(1 + ((thickness - TVL1) / TVLe))
-    return B
+    data = {
+        'Wall': wall,
+        'Energy': energy_choice,
+        'P (mSv/wk)': P,
+        'T': T,
+        'F (cm^2)': F,
+        'Distance (m)': dist,
+        'Scatter Distance (m)': d_sca,
+    }
 
+    thickness = 10.0  # starting value in cm
 
-def H_ps_unshielded(W, d_secondary, d_scatter, a, F):
-    # Calculates unshielded equivalent dose for PATIENT SCATTER
-    """
-    param W: workload in a typical week (Gy/wk)
-    param d_secondary: distance from isocenter to the point of interest (cm). Includes the 0.3 m
-    param d_scatter: distance from target to isocenter. Usually 1 m
-    param a: scatter fraction dependent on angle (deg) and beam energy (MV)
-    param F: field size in cm^2
-    """
-    H_ps_uns = (W * a * F) / (d_secondary ** 2 * d_scatter ** 2 * 400) * 10 ** 3
-    return H_ps_uns  # (mSv/wk)
+    if energy_choice in ['6', 'both']:
+        print("\n--- 6 MV Inputs ---")
+        TVLa = float(input("6 MV Patient Scatter TVL (cm): "))
+        W_a = float(input("6 MV Workload (Gy/wk): "))
+        a_a = float(input("6 MV Average scatter fraction: "))
+        TVL1_c = float(input("6 MV Leakage TVL_1 (cm): "))
+        TVLe_c = float(input("6 MV Leakage TVL_e (cm): "))
+        W_c = float(input("6 MV Leakage Workload (Gy/wk): "))
 
+        data.update({
+            '6MV_TVL': TVLa,
+            '6MV_Workload': W_a,
+            '6MV_Scatter_Frac': a_a,
+            '6MV_TVL1_Leak': TVL1_c,
+            '6MV_TVLe_Leak': TVLe_c,
+            '6MV_Leak_Workload': W_c,
+        })
+    else:
+        data.update({
+            '6MV_TVL': '',
+            '6MV_Workload': '',
+            '6MV_Scatter_Frac': '',
+            '6MV_TVL1_Leak': '',
+            '6MV_TVLe_Leak': '',
+            '6MV_Leak_Workload': '',
+        })
 
-def H_leak_unshielded(W_L, d_leakage):
-    # Calculates UNSHIELDED equivalent dose for LEAKAGE
-    """
-    param W_L: leakage workload (Gy/wk)
-    param d_leakage: distance from source of leakage to point of protection (cm)
-    """
-    H_leak_uns = ((W_L * 10 ** -3) / d_leakage ** 2) * 10 ** 3
-    # 10**-3 is the included barriers from the head and 10**3 converts Sv to mSv
-    return H_leak_uns  # (mSv/wk)
+    if energy_choice in ['18', 'both']:
+        print("\n--- 18 MV Inputs ---")
+        TVLb = float(input("18 MV Patient Scatter TVL (cm): "))
+        W_b = float(input("18 MV Workload (Gy/wk): "))
+        a_b = float(input("18 MV Average scatter fraction: "))
+        TVL1_d = float(input("18 MV Leakage TVL_1 (cm): "))
+        TVLe_d = float(input("18 MV Leakage TVL_e (cm): "))
+        W_d = float(input("18 MV Leakage Workload (Gy/wk): "))
 
+        data.update({
+            '18MV_TVL': TVLb,
+            '18MV_Workload': W_b,
+            '18MV_Scatter_Frac': a_b,
+            '18MV_TVL1_Leak': TVL1_d,
+            '18MV_TVLe_Leak': TVLe_d,
+            '18MV_Leak_Workload': W_d,
+        })
+    else:
+        data.update({
+            '18MV_TVL': '',
+            '18MV_Workload': '',
+            '18MV_Scatter_Frac': '',
+            '18MV_TVL1_Leak': '',
+            '18MV_TVLe_Leak': '',
+            '18MV_Leak_Workload': '',
+        })
 
-# If we assume uniform gantry rotation, you must find average scatter fraction and d_sec = d_L.
-def find_t_secondary(P, T, F, dist, d_sca):
-    """
-    :param P: design goal (mSv/wk)
-    :param T: occupancy factor
-    :param F: field size (cm^2)
-    :param dist: distance from iso to distal surface of wall. Does not include 0.3 m
-    :param d_sca: scatter distance (m) from source to point of scatter. Usually 1 m
-    :return:
-    """
-    wall = input("Which wall?  ")
-    print(f"Wall {wall}")
-    print("6 MV Patient Scatter")
-    TVLa = float(input("Input TVL (cm):  "))
-    W_a = float(input("Input workload: "))
-    a_a = float(input("Input average scatter fraction:  "))
+    while True:
+        H_total = 0
 
-    print("6 MV Leakage")
-    TVL1_c = float(input("Input TVL_1 (cm):  "))
-    TVLe_c = float(input("Input TVL_e (cm):  "))
-    W_c = float(input("Input workload: "))
+        if energy_choice in ['6', 'both']:
+            scatter_6MV = 10 ** -(thickness / TVLa) * (W_a * a_a * F) / (dist ** 2 * d_sca ** 2 * 400)
+            leak_6MV = 10 ** -(1 + ((thickness - TVL1_c) / TVLe_c)) * (W_c * 1e-3) / dist ** 2
+            H_total += scatter_6MV + leak_6MV
 
-    print("18 MV Patient Scatter")
-    TVLb = float(input("Input TVL (cm):  "))
-    W_b = float(input("Input workload: "))
-    a_b = float(input("Input average scatter fraction:  "))
+        if energy_choice in ['18', 'both']:
+            scatter_18MV = 10 ** -(thickness / TVLb) * (W_b * a_b * F) / (dist ** 2 * d_sca ** 2 * 400)
+            leak_18MV = 10 ** -(1 + ((thickness - TVL1_d) / TVLe_d)) * (W_d * 1e-3) / dist ** 2
+            H_total += scatter_18MV + leak_18MV
 
-    print("18 MV Leakage")
-    TVL1_d = float(input("Input TVL_1 (cm):  "))
-    TVLe_d = float(input("Input TVL_e (cm):  "))
-    W_d = float(input("Input workload: "))
+        H_total *= 1e3  # convert to mSv/wk
 
-    d_sec = dist
-    d_leak = dist
-    thickness = 10  # cm
+        if P / T > H_total:
+            break
+        thickness += 0.1
 
-    # Add QA workload
-    flag = True
-    while flag:
-        H_total_shielded = ((10 ** -(thickness / TVLa) * (W_a * a_a * F) / (d_sec ** 2 * d_sca ** 2 * 400)) +
-                            (10 ** -(thickness / TVLb) * (W_b * a_b * F) / (d_sec ** 2 * d_sca ** 2 * 400)) +
-                            (10 ** -(1 + ((thickness - TVL1_c) / TVLe_c)) * (W_c * 10 ** -3) / d_leak ** 2) +
-                            (10 ** -(1 + ((thickness - TVL1_d) / TVLe_d)) * (W_d * 10 ** -3) / d_leak ** 2)) * 10**3
-        if P/T > H_total_shielded:
-            flag = False
-            print('---------------')
-            print(f"Thickness {thickness} cm")
+    data['Required_Thickness_cm'] = round(thickness, 1)
 
-        else:
-            thickness += 0.1
+    # Write to CSV
+    file_exists = os.path.isfile(filename)
+    with open(filename, mode='a', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=data.keys())
+        if not file_exists:
+            writer.writeheader()
+        writer.writerow(data)
 
-    print(f"Design Goal {P} mSv/wk")
-    print(f"Occupancy Factor {T}")
-    print(f"Field Size {F}")
-    print(f"Distance {dist} m")
+    print(f"\nShielding thickness for wall '{wall}' recorded: {thickness:.1f} cm")
 
-
-find_t_secondary(0.1,0.5,1600,7.158,1)
+find_t_secondary(0.1, 0.5, 1600, 7.158, 1, filename='secondary_shielding.csv')
---
+++ Shielding Project.dwl
@@ -0,0 +1,3 @@
+david
+DAVID 
+Monday, April 21, 2025  6:08:08 PM
